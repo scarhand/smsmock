0b30098d1b45fd605a3cb2866abe10b407cb514b
Niels van der Zanden <niels@phusion.nl>
2015-05-29 21:24:56 +0200

    Updated api usage for twilio

--- lib/smsmock/twilio/client.rb
+++ lib/smsmock/twilio/client.rb
@@ -1,37 +1,8 @@
 module Twilio
   module REST
-    class Client < BaseClient
-      def initialize(account_sid, auth_token)
-        @account_sid = account_sid
-        @auth_token = auth_token
-      end
-
-      def account
-        @account ||= Account.new
-      end
-    end
-
-    class Account < InstanceResource
-      def initialize
-      end
-
-      def sms
-        @sms ||= Sms.new
-      end
-    end
-
-    class Sms < InstanceResource
-      def initialize
-      end
-
-      def messages
-        @messages ||= Messages.new
-      end
-    end
-
     class Messages < ListResource
       include SmsMock::Helpers
-      def initialize
+      def initialize(*args)
       end
 
       def create(opts={})
--- spec/smsmock_spec.rb
+++ spec/smsmock_spec.rb
@@ -4,13 +4,21 @@ describe SmsMock do
 
   before :each do
     @client = Twilio::REST::Client.new('','')
+    SmsMock::Client.messages.clear
   end
 
   it 'add a message to SmsMock::Client' do
-    @client.account.sms.messages.create(to: '123981', body: 'abc123', from: '1291')
+    @client.messages.create(to: '123456', body: 'abc123', from: '654312')
     expect(SmsMock::Client).to have_messages
     expect(SmsMock::Client.messages.last).to have_body 'abc123'
-    expect(SmsMock::Client.messages.last).to be_sent_from '1291'
-    expect(SmsMock::Client.messages.last).to be_sent_to '123981'
+    expect(SmsMock::Client.messages.last).to be_sent_from '654312'
+    expect(SmsMock::Client.messages.last).to be_sent_to '123456'
+  end
+
+  it 'adds multiple messages to SmsMock::Client' do
+    expect(SmsMock::Client).to have_messages(0)
+    @client.messages.create(to: '123456', body: 'abc123', from: '654321')
+    @client.messages.create(to: '567890', body: 'abc567', from: '098765')
+    expect(SmsMock::Client).to have_messages(2)
   end
 end
